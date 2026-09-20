255ab0c67e5b18ca51a73ec356543c576ee63390
Gowtham2029 <Sinki2029@gmail.com>
2026-09-20 20:44:27 +0530

    DB Connection --> check the Login controller flow

---
+++ DataProvider/UserProvider.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using MySqlConnector;
+using Practice_A.DataProvider;
+
+namespace Practice_A.DataProvider
+{
+    public class UserProvider : IUserProvider
+    {
+        IConfiguration _configuration;
+        public UserProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IEnumerable<string>>? GetUserName(string Email)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            // SET SQL_SAFE_UPDATES = 0; --> use this before delete, update commands
+
+            var sql = "select name from User";
+            using var connection = new MySqlConnection(connectionString);
+
+            var users = await connection.QueryAsync<string>(sql);
+            return users;
+        }
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Practice_A.DataProvider;
+namespace Practice_A.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        IUserProvider userProvider;
+        public UserRepository(IUserProvider _userProvider)
+        {
+            userProvider = _userProvider;
+        }
+        public async Task<IEnumerable<string>>? GetUserName(string Email)
+        {
+            var userName = await userProvider.GetUserName(Email);
+            return userName;
+        }
+    }
+}
